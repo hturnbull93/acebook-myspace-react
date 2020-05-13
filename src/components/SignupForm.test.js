e5adcb2d17323af997b75a390966cf4d1a144e88
Harry Turnbull <hturnbull@live.co.uk>
2020-05-13 16:53:14 +0100

    state.message is Logged In if successful fetch

--- src/components/SignupForm.test.js
+++ src/components/SignupForm.test.js
@@ -1,6 +1,7 @@
 // using enzyme
 import React from "react";
 import { shallow } from "enzyme";
+import waitUntil from 'async-wait-until';
 
 import SignupForm from "./SignupForm.js";
 
@@ -9,10 +10,7 @@ it("renders without crashing", () => {
 });
 
 it("form onSubmit sends fetch and parses json result", (done) => {
-  const mockSuccessResponse = {
-    status: 200,
-    loggedIn: true,
-  };
+  const mockSuccessResponse = {};
   const mockJsonPromise = Promise.resolve(mockSuccessResponse);
   const mockFetchPromise = Promise.resolve({
     json: () => mockJsonPromise,
@@ -33,6 +31,60 @@ it("form onSubmit sends fetch and parses json result", (done) => {
 
   expect(global.fetch).toHaveBeenCalledTimes(1);
 
+  global.fetch.mockClear();
   done();
 });
 
+it("form onSubmit sends displays logged in if credentials correct", async () => {
+  const mockSuccessResponse = {
+    status: 200,
+    loggedIn: true,
+    success: "Logged in",
+  };
+  const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+  const mockFetchPromise = Promise.resolve({
+    json: () => mockJsonPromise,
+  });
+
+  jest.spyOn(global, "fetch").mockImplementation(() => mockFetchPromise);
+
+  const wrapper = shallow(<SignupForm />);
+  const form = wrapper.find("form");
+
+  form.simulate("change", {
+    firstName: "tom",
+    lastName: "samson",
+    email: "tom@harry.com",
+    password: "password",
+  });
+  form.simulate("submit")
+  await waitUntil(() => wrapper.state('isSubmitting') === false)
+  expect(wrapper.state().message).toEqual("Logged in")
+});
+
+it("state.message is Logged In if successful fetch", async () => {
+  const mockSuccessResponse = {
+    status: 200,
+    loggedIn: true,
+    success: "Logged in",
+  };
+  const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+  const mockFetchPromise = Promise.resolve({
+    json: () => mockJsonPromise,
+  });
+
+  jest.spyOn(global, "fetch").mockImplementation(() => mockFetchPromise);
+
+  const wrapper = shallow(<SignupForm />);
+  const form = wrapper.find("form");
+
+  form.simulate("change", {
+    firstName: "tom",
+    lastName: "samson",
+    email: "tom@harry.com",
+    password: "password",
+  });
+  form.simulate("submit")
+  await waitUntil(() => wrapper.state('isSubmitting') === false)
+  expect(wrapper.state().message).toEqual("Logged in")
+});
