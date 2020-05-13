e756055d76e4a83ea9f7c1a75d8fb9c1f781219a
Harry Turnbull <hturnbull@live.co.uk>
2020-05-13 15:36:57 +0100

    replaced form with SignupForm component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,30 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import SignupForm from './SignupForm'
 
 export class Signup extends Component {
   constructor(props) {
     super(props);
-    this.state = { sent: "" }
+    this.state = { sent: "" };
   }
-  sendForm = (event) => {
-    if(event) event.preventDefault()
-    this.setState({ sent: "Success"})
-  }
-  
+
   render() {
     return (
       <div>
         <h1>Sign up</h1>
-        <form onSubmit={this.sendForm}>
-          <input type="text" name="firstName"/>
-          <input type="text" name="lastName"/>
-          <input type="text" name="email"/>
-          <input type="text" name="password"/>
-          <input type="submit" className="submit"/>
-        </form>
-    <div>{this.state.sent}</div>
+        <SignupForm/>
       </div>
-    )
+    );
   }
 }
 
-export default Signup
+export default Signup;
