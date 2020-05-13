1b5958f2cfd8865b1821129060154857f3dd19be
tommyrharper <tommyrharper@gmail.com>
2020-05-13 11:54:28 +0100

    Navigation component links different routes on the home page

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+import { NavLink } from 'react-router-dom';
+ 
+const Navigation = () => {
+    return (
+       <div>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/posts">Posts</NavLink>
+       </div>
+    );
+}
+ 
+export default Navigation;
