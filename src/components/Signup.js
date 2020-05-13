35d1fa1e6c1539dc3d332dc984b8d5a8840979fa
tommyrharper <tommyrharper@gmail.com>
2020-05-13 14:27:57 +0100

    Sign up form created

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
 
 export class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { sent: "" }
+  }
+  sendForm = (event) => {
+    if(event) event.preventDefault()
+    this.setState({ sent: "Success"})
+  }
+  
   render() {
     return (
       <div>
         <h1>Sign up</h1>
+        <form onSubmit={this.sendForm}>
+          <input type="text" name="firstName"/>
+          <input type="text" name="lastName"/>
+          <input type="text" name="email"/>
+          <input type="text" name="password"/>
+          <input type="submit" className="submit"/>
+        </form>
+    <div>{this.state.sent}</div>
       </div>
     )
   }
