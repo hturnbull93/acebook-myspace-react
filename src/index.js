96f9dce5f1fd7dd92e51be4bfad5c36d5a38fd9b
tommyrharper <tommyrharper@gmail.com>
2020-05-13 11:51:56 +0100

    Replaced PostsPage with App component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PostPage from './PostPage.js'
+import App from './App.js';
 
 
-ReactDOM.render(<PostPage />, document.getElementById("root"));
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
