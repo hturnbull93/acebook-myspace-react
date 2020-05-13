c434dd6b066d8fe8cca5a8f3707ed88670a5e845
tommyrharper <tommyrharper@gmail.com>
2020-05-13 14:28:22 +0100

    tests signup has a form in it

--- src/components/Signup.test.js
+++ src/components/Signup.test.js
@@ -14,3 +14,12 @@ it("has h1 with Sign up in it (enzyme)", () => {
   expect(wrapper).toContainReact(title);
 });
 
+it("has Sign up form in it (enzyme)", () => {
+  const wrapper = shallow(<Signup />);
+  const form =  wrapper.find("form");
+  form.simulate("change", {firstName: "tom", lastName: "samson", email: "tom@harry.com", password: "password"});
+  expect(wrapper).not.toContainReact(<div>Success</div>)
+  form.simulate('submit');
+  expect(wrapper).toContainReact(<div>Success</div>)
+});
+
