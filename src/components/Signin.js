8f880809504a8376e6ba7dde4dc8f1999a61c45c
tommyrharper <tommyrharper@gmail.com>
2020-05-13 17:38:56 +0100

    Sign in page create Parent component of SigninForm created

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,60 +1,18 @@
 import React, { Component } from "react";
+import SigninForm from './SigninForm'
 
 export class Signin extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      values: {
-        email: "",
-        password: "",
-      },
-      isSubmitting: false,
-      isError: false,
-    };
+    this.state = { sent: "" };
   }
-  handleInputChange = (e) =>
-    this.setState({
-      values: { ...this.state.values, [e.target.name]: e.target.value },
-    });
 
-  submitForm = async (e) => {
-    if (e) e.preventDefault();
-    this.setState({ isSubmitting: true });
-    const res = await fetch("/example-posts.json", {
-      method: "POST",
-      body: JSON.stringify(this.state.values),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    this.setState({ isSubmitting: false });
-    const data = await res.json();
-    !data.hasOwnProperty("error")
-      ? this.setState({ message: data.success })
-      : this.setState({ message: data.error, isError: true });
-  };
   render() {
     return (
-      <form onSubmit={this.submitForm}>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          onChange={this.handleInputChange}
-          value={this.state.values.email}
-          required
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          onChange={this.handleInputChange}
-          value={this.state.values.password}
-          required
-        />
-        <input type="submit" className="submit" />
-
-      </form>
+      <div>
+        <h1>Sign in</h1>
+        <SigninForm/>
+      </div>
     );
   }
 }
