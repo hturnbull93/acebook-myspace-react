2ddd65cd3776fdfede324e3d5fa78818c66a4e49
tommyrharper <tommyrharper@gmail.com>
2020-05-15 15:19:45 +0100

    advanced hompage created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,19 @@
-import React from 'react';
- 
-const home = () => {
-    return (
-       <div>
-          <h1>Home</h1>
-           <p>Home page body content</p>
-       </div>
-    );
+import React, { Component } from 'react';
+import SignupForm from './SignupForm'
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+    render() {
+        return (
+            <div>
+               <h1>Home</h1>
+                <h1> Status: { this.props.loggedInStatus }</h1>
+                <p>Welcome to Acebook Myspace</p>
+            </div>
+         );
+    }
 }
  
-export default home;
