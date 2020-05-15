6fcca74ccacf9a345e1d5bc24abea1863dd04b7d
tommyrharper <tommyrharper@gmail.com>
2020-05-15 16:18:42 +0100

    Only shows create post if logged in

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,14 +2,14 @@ import React from 'react';
  
 import { NavLink } from 'react-router-dom';
  
-const Navigation = () => {
+const Navigation = (props) => {
     return (
        <div>
           <NavLink to="/">Home</NavLink>
           <NavLink to="/posts">Posts</NavLink>
           <NavLink to="/signup">Sign up</NavLink>
           <NavLink to="/signin">Sign in</NavLink>
-          <NavLink to="/createpost">Create Post</NavLink>
+          { props.loggedInStatus === 'LOGGED_IN' ? <NavLink to="/createpost">Create Post</NavLink> : null }
        </div>
     );
 }
